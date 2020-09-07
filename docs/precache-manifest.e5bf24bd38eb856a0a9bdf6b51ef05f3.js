0a4111ca7096fa0dc2291b0591767c966e222e6e
Amaury <amaurysunstar@gmail.com>
2020-09-06 19:29:41 -0500

    build

---
+++ docs/precache-manifest.e5bf24bd38eb856a0a9bdf6b51ef05f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06bebf919d0159ea6b1ab5e9423b41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74d5eb65e763b9ad8a3",
+    "url": "/static/css/main.435066e6.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74d5eb65e763b9ad8a3",
+    "url": "/static/js/main.560505ac.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
